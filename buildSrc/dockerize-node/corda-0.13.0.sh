74d26550e68440a7306d30bfafebcaf5aad7cb92
Balaji <labsji@gmail.com>
2017-07-29 21:30:51 +0530

    startup corda-webserver too

--- buildSrc/dockerize-node/corda-0.13.0.sh
+++ buildSrc/dockerize-node/corda-0.13.0.sh
@@ -38,5 +38,6 @@ rpcUsers=[
 ]
 EOF
 
+exec java $JAVA_OPTIONS -jar /opt/corda/corda-webserver.jar >>/opt/corda/logs/web-output.log 2>&1
 exec java $JAVA_OPTIONS -Dcapsule.jvm.args="$CAPSULE_ARGS" -jar /opt/corda/corda.jar >>/opt/corda/logs/output.log 2>&1
 
