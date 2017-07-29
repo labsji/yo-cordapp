21bb713c8228a694a24a71b6447fb873afc71635
Balaji <labsji@gmail.com>
2017-07-29 21:36:01 +0530

    installed tmux, started webserver under tmux

--- buildSrc/dockerize-node/corda-0.13.0.sh
+++ buildSrc/dockerize-node/corda-0.13.0.sh
@@ -3,6 +3,7 @@
 export CORDA_HOST="${CORDA_HOST:-localhost}"
 export CORDA_PORT_P2P="${CORDA_PORT_P2P:-10002}"
 export CORDA_PORT_RPC="${CORDA_PORT_RPC:-10003}"
+export CORDA_PORT_WEB="${CORDA_PORT_WEB:-10004}"
 export CORDA_LEGAL_NAME="${CORDA_LEGAL_NAME:-Corda Test Node}"
 export CORDA_ORG="${CORDA_ORG:-CordaTest}"
 export CORDA_ORG_UNIT="${CORDA_ORG_UNIT:-CordaTest}"
@@ -18,6 +19,7 @@ cat > node.conf << EOF
 basedir : "/opt/corda"
 p2pAddress : "$CORDA_HOST:$CORDA_PORT_P2P"
 rpcAddress : "$CORDA_HOST:$CORDA_PORT_RPC"
+webAddress : "$CORDA_HOST:$CORDA_PORT_WEB"
 h2port : 11000
 myLegalName : "CN=$CORDA_LEGAL_NAME,O=$CORDA_ORG,OU=$CORDA_ORG_UNIT,L=$CORDA_CITY,C=$CORDA_COUNTRY, E=$CORDA_EMAIL"
 keyStorePassword : "cordacadevpass"
@@ -38,6 +40,6 @@ rpcUsers=[
 ]
 EOF
 
-exec java $JAVA_OPTIONS -jar /opt/corda/corda-webserver.jar >>/opt/corda/logs/web-output.log 2>&1
 exec java $JAVA_OPTIONS -Dcapsule.jvm.args="$CAPSULE_ARGS" -jar /opt/corda/corda.jar >>/opt/corda/logs/output.log 2>&1
+exec java $JAVA_OPTIONS -jar /opt/corda/corda-webserver.jar >>/opt/corda/logs/web-output.log 2>&1
 
